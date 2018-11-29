c96abf1f7b6d85bf0f39fdb25e2f0e01a614bdaf
g800897 <taryn.chambers@syniverse.com>
2018-11-29 14:51:44 -0500

    install cucumber, chai, chai-as-promised, protractor-cucumber-framework

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to protractor-cucumber-chai!');
+    expect(page.getTitleText()).toEqual('Protractor, Cucumber, Chai testing');
   });
 });
